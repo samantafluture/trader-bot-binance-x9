01d0cd7ee573dd6745ec66fd5973316b64211fdf
Samanta Fluture <samantafluture@gmail.com>
2021-11-24 16:03:14 -0200

    integrate bot with telegram

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// ETHBUSD
+// min 4129
+// max 4314
+
+// token -> 2113568022:AAGOSCqw3AGnQBx2mHS-WA7oA6kSILcvdK0
+// chat id -> 2102300440
+
 const WebSocket = require('ws');
 const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1h');
+
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('2113568022:AAGOSCqw3AGnQBx2mHS-WA7oA6kSILcvdK0');
 
 ws.onmessage = (event) => {
     const data = JSON.parse(event.data);
@@ -7,16 +17,16 @@ ws.onmessage = (event) => {
     console.log('Current Value ' + valor);
 
     if (valor >= 4314) {
+        bot.telegram.sendMessage(2102300440, 'ETHBUSD: ' + valor + ' - Time to Sell!');
         console.log('Sell');
     }
     else if (valor <= 4129) {
+        bot.telegram.sendMessage(2102300440, 'ETHBUSD: ' + valor + ' - Time to Buy!');
         console.log('Buy');
     }
 }
 
-// ETHBUSD
-// min 4129
-// max 4314
 console.log("bot connected");
+
 
 
